2803b9039d6ab4eb009ba0a30c3d9ca7d79951ea
rvogel <vogel@hallowelt.biz>
2020-11-19 18:08:19 +0100

    Rework logic
    * Use dedicated library to access LDAP that allows pages results
    * Re-implement user-info/group sync
    * Add domain db sync
    
    Change-Id: Iecd7270291e66d5bf6fbeceee9e0b66dd31a1539

--- src/UsersSyncMechanism.php
+++ src/UsersSyncMechanism.php
@@ -1,16 +1,22 @@
 <?php
+
 namespace LDAPSyncAll;
 
-use Config;
 use Exception;
 use IContextSource;
+use Wikimedia\Rdbms\LoadBalancer;
+use LDAPSyncAll\UserListProvider\LdapToolsBackend;
+use MediaWiki\Extension\LDAPAuthorization\Config as LDAPAuthorizationConfig;
+use MediaWiki\Extension\LDAPAuthorization\RequirementsChecker;
 use MediaWiki\Extension\LDAPGroups\Config as LDAPGroupsConfig;
 use MediaWiki\Extension\LDAPGroups\GroupSyncProcess;
+use MediaWiki\Extension\LDAPProvider\Client;
+use MediaWiki\Extension\LDAPProvider\ClientConfig;
 use MediaWiki\Extension\LDAPProvider\ClientFactory;
 use MediaWiki\Extension\LDAPProvider\DomainConfigFactory;
+use MediaWiki\Extension\LDAPProvider\UserDomainStore;
 use MediaWiki\Extension\LDAPUserInfo\UserInfoSyncProcess;
 use MediaWiki\Extension\LDAPUserInfo\Config as LDAPUserInfoConfig;
-use LoadBalancer;
 use Psr\Log\LoggerInterface;
 use SpecialBlock;
 use Status;
@@ -87,6 +93,27 @@ class UsersSyncMechanism {
 	public $addedUsersFailsCount = 0;
 
 	/**
+	 * @var DomainConfigFactory
+	 */
+	private $domainConfigFactory = null;
+
+	/**
+	 * @var array
+	 */
+	private $usernameDomainMap = [];
+
+	/**
+	 * @var UserDomainStore
+	 */
+	private $userDomainStore = null;
+
+	/**
+	 *
+	 * @var Client[]
+	 */
+	private $clients = null;
+
+	/**
 	 * UsersSyncMechanism constructor.
 	 * @param string[] $domains
 	 * @param array $LDAPGroupsSyncMechanismRegistry
@@ -115,172 +142,259 @@ class UsersSyncMechanism {
 		$this->logger = $logger;
 		$this->loadBalancer = $loadBalancer;
 		$this->context = $context;
+		$this->domainConfigFactory = DomainConfigFactory::getInstance();
+		$this->userDomainStore = new UserDomainStore( $this->loadBalancer );
 	}
 
 	/**
 	 * @return Status
 	 */
 	public function sync() {
-		$this->status = \Status::newGood();
-		$this->doSync();
-		$this->logger->alert(
-			'LDAPSyncAll completed. 
-			{addedUsersCount} users added; 
-			{disabledUsersCount} users disabled;
-			{addedUsersFailsCount} users failed to add;
-			{disabledUsersFailsCount} users failed to disable;',
-			[
+		try {
+			$this->doSync();
+
+			$results = [
 				'addedUsersCount' => $this->addedUsersCount,
 				'disabledUsersCount' => $this->disabledUsersCount,
 				'addedUsersFailsCount' => $this->addedUsersFailsCount,
 				'disabledUsersFailsCount' => $this->disabledUsersFailsCount
-			]
-		);
+			];
+			$this->logger->debug(
+				'LDAPSyncAll completed.
+				{addedUsersCount} users added;
+				{disabledUsersCount} users disabled;
+				{addedUsersFailsCount} users failed to add;
+				{disabledUsersFailsCount} users failed to disable;',
+				$results
+			);
+			$this->status = Status::newGood( $results );
+		}
+		catch ( Exception $ex ) {
+			$this->status = Status::newFatal( $ex->getMessage() );
+		}
+
 		return $this->status;
 	}
 
 	protected function doSync() {
-		$localUsers = $this->getUsersFromDB();
-		$ldapUsers = [];
-		try {
-			foreach ( $this->domains as $domain ) {
-				$ldapUsers += $this->getUsersFromLDAP( $domain );
-			}
-		} catch ( Exception $exception ) {
-			$this->logger->error(
-				'Fetching users from LDAP has been failed. Message: {message}',
-				[
-					'message' => $exception->getMessage()
-				]
-			);
-			return;
-		}
+		$this->initClients();
+		$this->syncLocalUserDB();
+		$this->updateUserDomainStore();
+		$this->syncAllUsersInfo();
+		$this->syncAllUsersGroups();
+	}
 
-		foreach ( $localUsers as $username => $user ) {
-			if ( !array_key_exists( $username, $ldapUsers ) ) {
-				$this->disableUser( $user );
-			}
-		}
-
-		foreach ( $ldapUsers as $ldapUsername => $ldapUser ) {
-			if ( !array_key_exists( $ldapUsername, $localUsers ) ) {
-				$this->addUser(
-					$ldapUser['user_name'],
-					$ldapUser['domain'],
-					$ldapUser['email'],
-					$ldapUser['user_real_name']
-				);
-			}
+	private function initClients() {
+		$factory = ClientFactory::getInstance();
+		foreach ( $this->domains as $domain ) {
+			$this->clients[$domain] = $factory->getForDomain( $domain );
 		}
 	}
 
-	/**
-	 * @param string $domain
-	 * @return array
-	 */
-	protected function getUsersFromLDAP( $domain ) {
-		$memberOf = '';
-		$authConfig = DomainConfigFactory::getInstance()
-			->factory( $domain, 'authorization' );
-		if ( $authConfig instanceof Config && $authConfig->has( 'rules' ) ) {
-			$rules = $authConfig->get( 'rules' );
-			if (
-				array_key_exists( 'groups', $rules ) &&
-				array_key_exists( 'required', $rules['groups'] )
-			) {
-				if ( count( $rules['groups']['required'] ) > 0 ) {
-					$memberOf = '(|';
-					foreach ( $rules['groups']['required'] as $group ) {
-						$memberOf .= '(memberOf=' . $group . ')';
-					}
-					$memberOf .= ')';
+	private function syncLocalUserDB() {
+		$usersToAddOrEnable = [];
+		$usersToDisable = [];
+		foreach ( $this->domains as $domain ) {
+			$userlistProvider = $this->makeUserListProvider( $domain );
+			$usernames = $userlistProvider->getWikiUsernames();
+			sort( $usernames );
+			foreach ( $usernames as $username ) {
+				$this->usernameDomainMap[$username] = $domain;
+				if ( $this->shouldAddOrEnable( $username, $domain ) ) {
+					$usersToAddOrEnable[] = $username;
+				} else {
+					$usersToDisable[] = $username;
 				}
 			}
 		}
 
-		$ldapUsersDirty = ClientFactory::getInstance()
-			->getForDomain( $domain )
-			->search( "(&(objectClass=User)(objectCategory=Person){$memberOf})" );
+		$localUsers = $this->getLocalDBUsers();
+		$localDBUsernames = array_keys( $localUsers );
+		// This will only disable users that are actually in the LDAP, but not "local users"
+		// TODO: fix
+		$usersToDisableLocally = array_intersect( $usersToDisable, $localDBUsernames );
 
-		$ldapUsers = [];
-		foreach ( $ldapUsersDirty as $user ) {
-			if ( $user['samaccountname'][0] ) {
-				$ldapUsers[User::getCanonicalName( $user['samaccountname'][0] )] = [
-					'user_name' => $user['samaccountname'][0],
-					'user_real_name' => $user['cn'][0],
-					'user_email' => $user['userprincipalname'][0],
-					'domain' => $domain
-				];
-			}
+		foreach ( $usersToDisableLocally as $username ) {
+			$userToDisable = $localUsers[$username];
+			$this->disableUser( $userToDisable );
 		}
 
-		return $ldapUsers;
+		foreach ( $usersToAddOrEnable as $username ) {
+			if ( isset( $localUsers[$username] ) ) {
+				$userToAddOrEnable = $localUsers[$username];
+			} else {
+				$userToAddOrEnable = User::newFromName( $username );
+			}
+			if ( $userToAddOrEnable->getId() !== 0 ) {
+				$this->maybeEnableUser( $userToAddOrEnable );
+			} else {
+				$this->addUser( $userToAddOrEnable );
+			}
+		}
+	}
+
+	private function updateUserDomainStore() {
+		$localUsers = $this->getLocalDBUsers();
+		foreach ( $this->usernameDomainMap as $username => $domain ) {
+			$user = null;
+			if ( isset( $localUsers[$username] ) ) {
+				$user = $localUsers[$username];
+			}
+			if ( $user === null ) {
+				continue;
+			}
+			if ( $user->isBlocked() ) {
+				continue;
+			}
+			if ( $user->getId() !== 0 ) {
+				$this->logger->error(
+					'Set domain for `{username}`',
+					[
+						'username' => $user->getName()
+					]
+				);
+				$this->userDomainStore->setDomainForUser( $user, $domain );
+			}
+		}
+	}
+
+	private function syncAllUsersInfo() {
+		$usersToSync = $this->getLocalDBUsers();
+		foreach ( $usersToSync as $user ) {
+			if ( $user->isBlocked() ) {
+				continue;
+			}
+			$this->logger->error(
+				'Sync info for `{username}`',
+				[
+					'username' => $user->getName()
+				]
+			);
+			$domain = $this->userDomainStore->getDomainForUser( $user );
+			$this->syncUserInfo( $user, $domain );
+		}
+	}
+
+	private function syncAllUsersGroups() {
+		$usersToSync = $this->getLocalDBUsers();
+		foreach ( $usersToSync as $user ) {
+			if ( $user->isBlocked() ) {
+				continue;
+			}
+			$this->logger->error(
+				'Sync groups for `{username}`',
+				[
+					'username' => $user->getName()
+				]
+			);
+			$domain = $this->userDomainStore->getDomainForUser( $user );
+			$this->syncUserGroups( $user, $domain );
+		}
 	}
 
 	/**
-	 * @return array
-	 */
-	protected function getUsersFromDB() {
-		$dbr = $this->loadBalancer->getConnection( DB_REPLICA );
-		$result = $dbr->select(
-			[ 'user', 'ipblocks' ],
-			[ 'user_id', 'user_name', 'ipb_expiry' ],
-			[ 'ipb_expiry is null' ],
-			__FUNCTION__,
-			[],
-			[
-				'ipblocks' => [ 'LEFT JOIN', 'user_id = ipb_user' ]
-			]
-		);
-
-		$users = [];
-
-		foreach ( $result as $row ) {
-			if ( in_array( $row->user_name, $this->excludedUsernames ) ) {
-				continue;
-			}
-			$user = User::newFromId( $row->user_id );
-			if ( !is_object( $user ) ) {
-				continue;
-			}
-			$users[$row->user_name] = $user;
-		}
-
-		return $users;
-	}
-
-	/**
+	 *
 	 * @param string $username
 	 * @param string $domain
-	 * @param string $email
-	 * @param string $realName
+	 * @return boolean
 	 */
-	protected function addUser( $username, $domain, $email = '', $realName = '' ) {
+	private function shouldAddOrEnable( $username, $domain ) {
+		$domainConfig = $this->domainConfigFactory->factory(
+			$domain,
+			LDAPAuthorizationConfig::DOMAINCONFIG_SECTION
+		);
+		$client = $this->clients[$domain];
+		$requirementsChecker = new RequirementsChecker( $client, $domainConfig );
+
+		$result = $requirementsChecker->allSatisfiedBy( $username );
+
+		return $result;
+	}
+
+	/**
+	 *
+	 * @return User[]
+	 */
+	private function getLocalDBUsers() {
+		$dbr = $this->loadBalancer->getConnection( DB_REPLICA );
+		$res = $dbr->select( 'user', '*', '', __METHOD__ );
+		$localUsers = [];
+		foreach ( $res as $row ) {
+			$localUser = User::newFromRow( $row );
+			$localUsers[$localUser->getName()] = $localUser;
+		}
+		return $localUsers;
+	}
+
+	/**
+	 *
+	 * @param string $domain
+	 * @return IUserListProvider
+	 */
+	private function makeUserListProvider( $domain ) {
+		$config = $this->domainConfigFactory->factory(
+			$domain,
+			ClientConfig::DOMAINCONFIG_SECTION
+		);
+
+		// TODO: Make registry
+		$userlistProvider = LdapToolsBackend::factory( $config );
+		return $userlistProvider;
+	}
+
+	/**
+	 * @param User $user
+	 */
+	private function addUser( $user ) {
 		try {
-			$user = User::newFromName( $username );
-			if ( $user->isBlocked() ) {
-				// TODO: enable?
-			} else {
-				$user->addToDatabase();
-
-				$user->setRealName( $realName );
-				$user->setEmail( $email );
-				$user->saveSettings();
-
-				$this->addedUsersCount++;
-			}
+			$this->logger->error(
+				'Add `{username}`',
+				[
+					'username' => $user->getName()
+				]
+			);
+			$user->addToDatabase();
+			$this->addedUsersCount++;
 		} catch ( Exception $exception ) {
 			$this->addedUsersFailsCount++;
-			$this->logger->alert(
+			$this->logger->error(
 				'User `{username}` creation error {message}',
 				[
-					'username' => $username,
+					'username' => $user->getName(),
 					'message' => $exception->getMessage()
 				]
 			);
 		}
-		$this->syncUserInfo( $user, $domain );
-		$this->syncUserGroups( $user, $domain );
+	}
+
+	/**
+	 * @param User $user
+	 */
+	private function maybeEnableUser( $user ) {
+		try {
+			if ( $user->isBlocked() ) {
+				$result = $user->getBlock()->delete();
+				$this->logger->debug( 'Enabling `{username}`: {result}',
+				[
+					'username' => $user->getName(),
+					'result' => $result ? 'OK' : 'FAIL'
+				] );
+			} else {
+				$this->logger->debug( 'Enabling `{username}`: NOT DISABLED',
+				[
+					'username' => $user->getName(),
+				] );
+			}
+		} catch ( Exception $exception ) {
+			$this->addedUsersFailsCount++;
+			$this->logger->error(
+				'Error while enabling `{username}`: {message}',
+				[
+					'username' => $user->getName(),
+					'message' => $exception->getMessage()
+				]
+			);
+		}
 	}
 
 	/**
@@ -288,9 +402,11 @@ class UsersSyncMechanism {
 	 * @param string $domain
 	 */
 	protected function syncUserGroups( User $user, $domain ) {
-		$client = ClientFactory::getInstance()->getForDomain( $domain );
-		$domainConfig = DomainConfigFactory::getInstance()
-			->factory( $domain, LDAPGroupsConfig::DOMAINCONFIG_SECTION );
+		$domainConfig = $this->domainConfigFactory->factory(
+			$domain,
+			LDAPGroupsConfig::DOMAINCONFIG_SECTION
+		);
+		$client = $this->clients[$domain];
 		$process = new GroupSyncProcess(
 			$user,
 			$domainConfig,
@@ -305,14 +421,15 @@ class UsersSyncMechanism {
 	 * @param string $domain
 	 */
 	protected function syncUserInfo( User $user, $domain ) {
+		$domainConfig = $this->domainConfigFactory->factory(
+			$domain,
+			LDAPUserInfoConfig::DOMAINCONFIG_SECTION
+		);
+		$client = $this->clients[$domain];
 		$process = new UserInfoSyncProcess(
 			$user,
-			DomainConfigFactory::getInstance()
-				->factory(
-					$domain,
-					LDAPUserInfoConfig::DOMAINCONFIG_SECTION
-				),
-			ClientFactory::getInstance()->getForDomain( $domain ),
+			$domainConfig,
+			$client,
 			$this->LDAPUserInfoModifierRegistry
 		);
 		$process->run();
@@ -326,6 +443,10 @@ class UsersSyncMechanism {
 			if ( UserGroupMembership::getMembership( $user->getId(), $group ) ) {
 				return;
 			}
+		}
+
+		if ( $user->isBlocked() ) {
+			return;
 		}
 
 		$data = [
@@ -347,16 +468,25 @@ class UsersSyncMechanism {
 			'Tags' => [ 'ldap' ],
 		];
 
-		if ( SpecialBlock::processForm( $data, $this->context ) ) {
+		$result = SpecialBlock::processForm( $data, $this->context );
+		if ( $result === true ) {
 			$this->disabledUsersCount++;
-			$this->logger->debug(
-				'User {username} has been disabled by UsersSyncMechanism',
-				[
-					'username' => $user->getName()
-				]
-			);
 		} else {
 			$this->disabledUsersFailsCount++;
+			$this->logger->error(
+				'Error while disabling user {username}: {message}',
+				[
+					'username' => $user->getName(),
+					'message' => $result
+				]
+			);
 		}
+		$this->logger->debug(
+			'Disabling `{username}`: {result}',
+			[
+				'username' => $user->getName(),
+				'result' => $result ? 'OK' : 'FAIL'
+			]
+		);
 	}
 }
