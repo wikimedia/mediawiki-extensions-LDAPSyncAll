596e9aa60289214f43b2d5609488f4a5d99a7ff8
rvogel <vogel@hallowelt.biz>
2020-11-19 18:08:19 +0100

    Rework logic
    * Use dedicated library to access LDAP that allows pages results
    * Re-implement user-info/group sync
    * Add domain db sync
    
    Change-Id: Iecd7270291e66d5bf6fbeceee9e0b66dd31a1539

--- README.md
+++ README.md
@@ -10,14 +10,14 @@ This extension provides mechanism which synchronizes users in database and users
 Execute
 
 	composer require hallowelt/ldapsyncall dev-REL1_31
-within MediaWiki root or add `hallowelt/ldapsyncall` to the
+within MediaWiki root or add `mediawiki/ldap-sync-all` to the
 `composer.json` file of your project
 
 ## Activation
 Add
 
 	wfLoadExtension( 'LDAPSyncAll' );
-to your `LocalSettings.php` or the appropriate `settings.d/` file.
+to your `LocalSettings.php`.
 
 ## Usage
 
---
+++ src/LdapTools/Log/MediaWikiLog.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace LDAPSyncAll\LdapTools\Log;
+
+use LdapTools\Log\LdapLoggerInterface as LogLdapLoggerInterface;
+use LdapTools\Log\LogOperation;
+use LdapTools\Operation\CacheableOperationInterface;
+use MediaWiki\Logger\LoggerFactory;
+use Psr\Log\LoggerInterface;
+
+class MediaWikiLog implements LogLdapLoggerInterface {
+
+	/**
+	 *
+	 * @var LoggerInterface
+	 */
+	private $logger = null;
+
+	public function __construct() {
+		$this->logger = LoggerFactory::getInstance( 'LDAPSyncAll' );
+	}
+	/**
+	 * The start method is called against a LDAP operation prior to the operation being executed.
+	 *
+	 * @param LogOperation $operation
+	 */
+	public function start( LogOperation $operation ) {
+		$message = "(".$operation->getDomain().") -- Start Operation Type: "
+			.$operation->getOperation()->getName().PHP_EOL;
+
+		foreach ( $operation->getOperation()->getLogArray() as $key => $value ) {
+			$message .= "\t$key: $value".PHP_EOL;
+		}
+
+		$this->logger->debug( $message );
+	}
+
+	/**
+	 * The end method is called against a LDAP operation after the operation has finished executing.
+	 *
+	 * @param LogOperation $operation
+	 */
+	public function end( LogOperation $operation ) {
+		$duration = $operation->getStopTime() - $operation->getStartTime();
+
+		if ( $operation->getOperation() instanceof CacheableOperationInterface ) {
+			echo "\tCache Hit: ".var_export( $operation->getUsedCachedResult(), true ).PHP_EOL;
+		}
+		if ( !is_null( $operation->getError() ) ) {
+			echo "\tError: ".$operation->getError().PHP_EOL;
+		}
+
+		$message = "(".$operation->getDomain().") -- End Operation Type: "
+			.$operation->getOperation()->getName()." -- ($duration seconds)".PHP_EOL;
+
+		$this->logger->debug( $message );
+	}
+}
