35a53f19f557cae6c912d39c9a24ae9b56488426
Hamish <hamish.slater@hallowelt.com>
2026-07-22 17:00:56 +0700

    phpcs
    Change-Id: Ied6b38767632442fecb03e9341bf251892cfb6dd

--- src/UserSyncMechanism/DisableUsersSyncMechanism.php
+++ src/UserSyncMechanism/DisableUsersSyncMechanism.php
@@ -55,6 +55,9 @@ class DisableUsersSyncMechanism extends UsersSyncMechanism {
 		$this->domainUserListProviders = $domainUserListProviders;
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function sync() {
 		try {
 			$this->doSync();
